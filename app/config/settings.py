136950bfd61af07dc1f8c921bc4746e5e22de933
MitoNacho <mitonacho9@gmail.com>
2026-05-13 20:04:45 +0200

    Fix Railway database URL

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,13 +1,36 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
 
 class Settings:
+    """
+    Configuración global del proyecto.
+    """
 
-    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "")
+    BOT_TOKEN: str = os.getenv(
+        "BOT_TOKEN",
+        "",
+    )
+
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL",
+        "",
+    )
+
+    # Compatibilidad Railway + SQLAlchemy
+    if DATABASE_URL.startswith(
+        "postgresql://"
+    ):
+
+        DATABASE_URL = DATABASE_URL.replace(
+            "postgresql://",
+            "postgresql+psycopg2://",
+            1,
+        )
 
 
 settings = Settings()
